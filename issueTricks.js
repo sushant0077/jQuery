6744afc5b43976bf9632c7c7ddd853d2353505fa
Sushant shewane <sushant0077@gmail.com>
2017-05-30 09:49:23 +0530

    Cross domain example

--- issueTricks.js
+++ issueTricks.js
@@ -6,3 +6,28 @@
 jQuery(".item iframe").each(function() {
 	jQuery(this)[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
 });
+
+
+/*cross domain filter for ajax*/
+jQuery.ajaxPrefilter( function (options) {
+	console.log(options);
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
+
+/* check weather your site in HTTP or HTTPS */
+var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+
+
+/* Ajax Example for cross domain site.*/
+jQuery.ajax({
+ 	type: "POST",
+	url: http + '//cors-anywhere.herokuapp.com/http://example.com',
+	data: Yourdata,
+	success: function (data) {
+		/* your logic goes here */
+	}
+});
